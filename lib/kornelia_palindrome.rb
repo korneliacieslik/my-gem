5b7ea1cf4d1b5d57e89ee9e122bc7328d5ea8dea
Kornelia <korneliacieslik08@gmail.com>
2020-11-10 13:42:24 +0100

    Initialize repository

---
+++ lib/kornelia_palindrome.rb
@@ -0,0 +1,6 @@
+require "kornelia_palindrome/version"
+
+module KorneliaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kornelia_palindrome/version.rb
@@ -0,0 +1,3 @@
+module KorneliaPalindrome
+  VERSION = "0.1.0"
+end
