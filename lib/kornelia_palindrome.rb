612301866d9bea1269bcd62047f211c869c9b6ec
Kornelia <korneliacieslik08@gmail.com>
2020-12-04 16:52:32 +0100

    Finish palindrome methode

--- lib/kornelia_palindrome.rb
+++ lib/kornelia_palindrome.rb
@@ -1,6 +1,15 @@
 require "kornelia_palindrome/version"
 
-module KorneliaPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String 
+  
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private 
+ 
+   def processed_content
+     downcase.scan(/[a-zA-Z]+/).join
+   end 
+
 end
